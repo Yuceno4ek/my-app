82e6b84d34b3b55ba4cb2a09ec621451c5fcc730
Yuceno4ek <yuceno4ek@gmail.com>
2022-12-14 14:27:17 +0200

    Added styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "./App.css";
 import Weather from "./Weather";
 
 const rootElement = document.getElementById("root");
@@ -9,5 +9,9 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <Weather />
+    <footer>
+      <a href="https://github.com/Yuceno4ek/my-app">Open source-code</a> by
+      Yuliia Demchenko
+    </footer>
   </StrictMode>
 );
